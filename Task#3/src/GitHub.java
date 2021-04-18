3e0d06f6d01b2cbc0c4f7e2a97136858d3f0999d
Yash Potbhare <Yash Potbhare@PC-ofYash12>
2021-04-18 15:20:45 +0530

    commit commit

---
+++ Task#3/src/GitHub.java
@@ -0,0 +1,16 @@
+package gitHub;
+import java.util.Scanner;
+public class GitHub {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter a no. of minutes");
+	int minutes=sc.nextInt();
+	int years = minutes / 525600;
+	int days = (minutes / 60 / 24 ) % 365;
+	
+	
+	System.out.println( years + "years " + "  " + days + "days " );
+	}
+
+}
